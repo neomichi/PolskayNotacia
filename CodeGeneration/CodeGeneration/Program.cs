4f4d0ed5823b94889fae728c21913527b92c819a
Николай <tneomichi@gmail.com>
2019-01-24 22:06:07 +0500

    Готово

---
+++ CodeGeneration/CodeGeneration/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeGeneration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var row = Console.ReadLine();
+            var response = "";
+            using (Microsoft.CSharp.CSharpCodeProvider test = new Microsoft.CSharp.CSharpCodeProvider())
+            {
+                var res = test.CompileAssemblyFromSource(
+                    new System.CodeDom.Compiler.CompilerParameters()
+                    {
+                        GenerateInMemory = true
+                    },
+                    "public class TestClass { public int Calc() { return "+ row + ";}}"
+                );
+
+                var type = res.CompiledAssembly.GetType("TestClass");
+                var obj = Activator.CreateInstance(type);
+                var output = type.GetMethod("Calc").Invoke(obj, new object[] { });
+                response = output.ToString();
+            }       
+            Console.WriteLine(response);           
+            Console.ReadKey();
+
+        }
+
+        /// зачем нам обратная польская нотация,это то же С#///
+
+
+    }
+}
